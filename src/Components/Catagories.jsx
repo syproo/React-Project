f94138ae57b53cb35cd28c93f5e99bf9516c904f
Muhammad Aousaf <ally666669991@gmail.com>
2023-07-15 15:10:05 +0500

    add catagories

---
+++ src/Components/Catagories.jsx
@@ -0,0 +1,137 @@
+import React from "react"
+import "./catagories.css"
+
+
+
+export const Catagories = () => {
+
+    return (
+        <div className="main">
+            <h1 className="head1">Top Brands</h1>
+
+            <div className="submain">
+
+                <div className="items">
+                    <img className="seth" src="src/assets/iphone-13.jpeg" alt="" width={300} height={300} />
+                    <div className="set">
+                        <h1 className="head2">Iphone-13 <br /><p className="para">1400 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+                        
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/sony.png" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">Sony Headphones<br /><p className="para">200 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/pistal.png1" alt="" width={900} />
+                    <div className="set">
+                        <h1 className="head2">Weapones<br /><p className="para">860 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/pokemon.png" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">Pokemom<br /><p className="para">100 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        
+            <h1 className="head1">Man's T Shirts</h1>
+
+            <div className="submain">
+
+                <div className="items">
+                    <img className="seth" src="src/assets/rshirt.png" alt="" width={300} height={300} />
+                    <div className="set">
+                        <h1 className="head2">Mens polo shirt<br /><p className="para">24 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+                        
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/gshirt.png" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">blue TShirt<br /><p className="para">27 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/Aadidas.png" alt="" width={900} />
+                    <div className="set">
+                        <h1 className="head2">Adidas TShirts<br /><p className="para">40 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/wight.jpg" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">White Adidas<br /><p className="para">32 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        
+            <h1 className="head1">Shoes</h1>
+
+            <div className="submain">
+
+                <div className="items">
+                    <img className="seth" src="src/assets/1Shoes.png" alt="" width={300} height={300} />
+                    <div className="set">
+                        <h1 className="head2">Casual Shoes<br /><p className="para">140 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+                        
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/2shoes.png" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">Running Shoes<br /><p className="para">250 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/3shoes-png.png" alt="" width={900} />
+                    <div className="set">
+                        <h1 className="head2">Running Shoes<br /><p className="para">210 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+                <div className="items">
+                    <img className="seth" src="src/assets/4shoes.png" alt="" width={200} />
+                    <div className="set">
+                        <h1 className="head2">Men in black<br /><p className="para">150 $</p></h1>
+                        <button type="button"><img className="btn1" src="src/assets/cart.png" alt="" width={40} /></button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        
+
+        </div>
+    )
+
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import { Navbar } from "../Components/Navbar";
 import { Banner } from "../Components/Banner";
 import { BannerFooter } from "../Components/BannerFooter";
+import { Catagories } from "../Components/Catagories";
 
 export const Home = () => {
   return (
     <>
-    
+    < Catagories/>
       <Navbar />
       <Banner />
       <BannerFooter/>
