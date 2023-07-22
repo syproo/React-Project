9e9bf38600e65d22204ca1127a02edbcb2a4621c
Ahtasham13 <98253789+Ahtasham13@users.noreply.github.com>
2023-07-22 22:11:28 +0500

    Changes by Ahtasham

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,15 +3,16 @@ import { Banner } from "../Components/Banner";
 import { BannerFooter } from "../Components/BannerFooter";
 import { Catagories } from "../Components/Catagories";
 
+
 export const Home = () => {
   return (
     <>
-    
+
       <Navbar />
       <Banner />
-      <BannerFooter/>
-      <Catagories/>
-  
+      <BannerFooter />
+      <Catagories />
+
     </>
   );
 };
